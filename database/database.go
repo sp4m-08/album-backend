f22d9d59005ea24db601c44d0ed2392b1903fb3f
sp4m-08 <roysaunok@gmail.com>
2025-07-01 21:24:29 +0530

    database secured

--- database/database.go
+++ database/database.go
@@ -3,8 +3,10 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 	"rest/models"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,7 +14,14 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "postgresql://postgres.tybnqguzgkaugyaosuuz:UbEIACnr0hxHSIRE@aws-0-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require"
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable not set")
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
